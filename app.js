506aa7c7b55b96a09738c2bcee356442877128db
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2024-10-23 12:09:16 +0530

    creating different routes

--- app.js
+++ app.js
@@ -2,6 +2,11 @@ const express = require("express");
 const app = express();
 const mongoose = require("mongoose");
 const Listing = require("./models/listing.js")
+const path = require("path");
+
+app.set('view engine', 'ejs');
+app.set("views", path.join(__dirname, 'views'));
+app.use(express.urlencoded({ extended: true }));
 
 const Mongo_URL = "mongodb://127.0.0.1:27017/wanderlust"
 
@@ -13,22 +18,36 @@ main().then(() => {
     console.log("database connection successful")
 }).catch(err => console.log(err));
 
-app.get("/", (req, res) => {
+app.get("/", (res) => {
     res.send("hi , I am root");
 })
-app.get("/testListing", async (req, res) => {
-    let sampleListing = new Listing({
-        title: "My new Villa",
-        description: "By the beach",
-        price: 1200,
-        location: "Calangute, Goa",
-        country: "India"
-    });
+// app.get("/testListing", async (req, res) => {
+//     let sampleListing = new Listing({
+//         title: "My new Villa",
+//         description: "By the beach",
+//         price: 1200,
+//         location: "Calangute, Goa",
+//         country: "India"
+//     });
 
-    await sampleListing.save();
-    console.log("sample was saved .")
-    res.send("successful test")
+//     await sampleListing.save();
+//     console.log("sample was saved .")
+//     res.send("successful test")
+// })
+
+//index route
+app.get("/listing", async (req, res) => {
+    const allListing = await Listing.find({});
+    res.render("listings/index.ejs", { allListing });
 })
+
+app.get("/listing/:id", async (req, res) => {
+    let { id } = req.params;
+    const listing = await Listing.findById(id);
+    res.render("listings/show.ejs", { listing });
+
+})
+
 app.listen(8080, () => {
     console.log("server lis listening on port 8080");
 })
