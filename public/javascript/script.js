d9de149b65750c2be491cb36c4b1f101d7228fc9
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2024-10-28 04:15:55 +0530

    added  home page and 3D designs

---
+++ public/javascript/script.js
@@ -0,0 +1,5 @@
+import { Application } from '@splinetool/runtime';
+
+const canvas = document.getElementById('canvas3d');
+const app = new Application(canvas);
+app.load('https://prod.spline.design/3CA5Ytuou-lTYv9q/scene.splinecode');
