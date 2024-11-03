86c8c991ae4cee7731735b970fa32bac4ea7169c
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2024-11-03 19:11:46 +0530

    deleted a redundant file

--- public/javascript/script.js
+++
@@ -1,5 +0,0 @@
-import { Application } from '@splinetool/runtime';
-
-const canvas = document.getElementById('canvas3d');
-const app = new Application(canvas);
-app.load('https://prod.spline.design/3CA5Ytuou-lTYv9q/scene.splinecode');
