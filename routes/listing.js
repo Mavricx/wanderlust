5afd8761fa30bfa6e99cfe22ad21182d16486b4b
Priyansu Pattanayak <ppattanayakcodes@gmail.com>
2025-02-05 01:21:06 +0530

    restructuring routes with routers.route() and styling the rating feature

--- routes/listing.js
+++ routes/listing.js
@@ -6,27 +6,21 @@ const { isLoggedIn, isOwner, validateListing } = require("../api/middleware.js")
 const listingController = require("../controllers/listings.js")
 
 
-
-
-//all listing route
-router.get("/", wrapAsync(listingController.index));
+router.route("/")
+    .get(wrapAsync(listingController.index))//all listing route
+    .post(validateListing, isLoggedIn, wrapAsync(listingController.createListing))//post listing route
 
 //add new listing route
 router.get("/new", isLoggedIn, listingController.renderNewForm);
 
-//show each listing route
-router.get("/:id", wrapAsync(listingController.showListing))
+router.route("/:id")
+    .get(wrapAsync(listingController.showListing))  //show each listing route
+    .put(isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing)) //put / edit route
+    .delete(isLoggedIn, isOwner, wrapAsync(listingController.destroyListing)) //delete route
+
 
 //get edit listing route
 router.get("/:id/edit", isLoggedIn, isOwner, wrapAsync(listingController.renderEditForm))
 
-//post listing route
-router.post("/", validateListing, isLoggedIn, wrapAsync(listingController.createListing))
-
-//put / edit route
-router.put("/:id", isLoggedIn, isOwner, validateListing, wrapAsync(listingController.updateListing))
-
-//delete route
-router.delete("/:id", isLoggedIn, isOwner, wrapAsync(listingController.destroyListing))
 
 module.exports = router;
